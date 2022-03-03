6081c10e4214b4de670a13b52221e8122d791355
lovurself <lovurself_44@naver.com>
2022-03-03 17:14:30 +0900

    React practice - useReducer을 이용한 Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
-{/* useState를 이용해서 바뀌는 값을 관리할 수 있다 */}
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            throw new Error('Unhandled action');
+    }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(0);
-    {/* number를 만들건데 기본값은 0이고 setNumber는 number을 바꿔주는 함수 */}
+    
+    const [number, dispatch] = useReducer(reducer, 0);
+
     const onIncrease = () => {
-        setNumber(number + 1);
+        dispatch({
+            type: 'INCREMENT'
+        })
     }
     const onDecrease = () => {
-        setNumber(prevNumber => prevNumber - 1);
-        {/* 함수형으로 업데이트(최적화와 관련이 있다) */}
+        dispatch({
+            type: 'DECREMENT'
+        })
     }
 
     return (
@@ -17,7 +31,6 @@ function Counter() {
             <h1>{number}</h1>
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
-            {/* react에서는 onIncrease()로 입력하면 함수가 호출됨, 함수를 넣어줘야지 함수를 호출하면 안된다*/}
         </div>
     );
 }
