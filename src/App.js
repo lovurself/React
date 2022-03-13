4e48217dd65ed8c5785093fd71146e48a664eb87
lovurself <lovurself_44@naver.com>
2022-03-13 22:35:30 +0900

    API 연동 - axios 라이브러리, useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,78 +1,9 @@
-import React, { useRef, useReducer, useMemo, useCallback, createContext } from "react";
-import CreateUser from "./CreateUser";
-import UserList from "./UserList";
-import useInputs from "./useInputs";
-
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는 중...');
-  return users.filter(user => user.active).length;
-}
-
-const initialState = {
-  users: [
-    {
-      id: 1,
-      username: "jully",
-      email: "jully@example.com",
-      active: true
-    },
-    {
-      id: 2,
-      username: "poter",
-      email: "poter@example.com",
-      active: false
-    },
-    {
-      id: 3,
-      username: "json",
-      email: "json@example.com",
-      active: false
-    }
-  ]
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
-
-    case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id
-            ? { ...user, active: !user.active }
-            : user
-        )
-      };
-
-    case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      }
-    default:
-      throw new Error('Unhandled action');
-  };
-};
-
-export const UserDispatch = createContext(null);
+import React from 'react';
+import Users from './Users';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const { users } = state;
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    <UserDispatch.Provider value={dispatch}>
-      <CreateUser />
-      <UserList users={users} />
-      <div>활성사용자 수 : {count}</div>
-    </UserDispatch.Provider>
+    <Users />
   );
 }
 
