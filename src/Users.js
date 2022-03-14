a944213f6ae22875b57241e21f80d2d66b1810b9
lovurself <lovurself_44@naver.com>
2022-03-14 21:44:07 +0900

    API 연동 - custom Hook (useAsync) 사용(컴포넌트 처음 렌더링에 데이터 요청)

--- src/Users.js
+++ src/Users.js
@@ -1,56 +1,15 @@
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
 
-// useReducer의 장점 : 따로 파일로 빼내서 재사용이 가능
-// 관리할 action 목록 : loading, success, error
-function reducer(state, action) {
-  switch(action.type) {
-    case 'LOADING':
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      }
-    case 'SUCCESS':
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      }
-    case 'ERROR':
-      return {
-        loading: false,
-        data: null,
-        error: action.error,
-      }
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+// callback으로 넣어줄 함수
+async function getUsers() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+  return response.data;
 }
 
 function Users() {
-
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null, 
-  });
-  const fetchUsers = async () => {
-    dispatch({ type: 'LOADING' });
-    try {
-        const response = await axios.get(
-          'https://jsonplaceholder.typicode.com/users/'
-        );
-        dispatch({ type: 'SUCCESS', data: response.data });
-    } catch (e) {
-      dispatch({ type: 'ERROR', error: e });
-    }
-  };
-
-  useEffect(() => {
-      fetchUsers();
-  }, []);
-
+  const [state, refetch] = useAsync(getUsers, []);
   const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩중...</div>
@@ -64,7 +23,7 @@ function Users() {
                 {user.username} ({user.name})
             </li>))}
         </ul>
-        <button onClick={fetchUsers}>다시 불러오기</button>
+        <button onClick={refetch}>다시 불러오기</button>
     </>
   )
 };
